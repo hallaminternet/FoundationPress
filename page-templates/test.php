6665517ea59100897cbb3ec8ece158219f182607
Alexander <a.koukovistas@outlook.com>
2018-02-16 12:01:49 +0000

    add post loop to the template:

--- page-templates/test.php
+++ page-templates/test.php
@@ -5,5 +5,15 @@ Template Name: Test
 
 $context = Timber::get_context();
 $post = new TimberPost();
-$context['post'] = $post;
+$context['page'] = $post;
+$args = array(
+// Get post type project
+    'post_type' => 'post',
+// Get all posts
+    'posts_per_page' => -1,
+// Order by post date
+    'orderby' => array(
+        'date' => 'DESC'
+    ));
+$context['posts'] = new Timber\PostQuery($args);
 Timber::render('/templates/test.twig',$context);
